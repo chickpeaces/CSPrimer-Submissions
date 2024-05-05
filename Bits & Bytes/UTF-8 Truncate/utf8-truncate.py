c0b084b59a60e2ac28520a9229adc940bb854c34
chickpeaces <chickpeaces+github@gmail.com>
2024-05-04 21:37:53 -1000

    Added utf8_truncate.py

--- Bits & Bytes/UTF-8 Truncate/utf8-truncate.py
+++ Bits & Bytes/UTF-8 Truncate/utf8-truncate.py
@@ -1,3 +1,20 @@
 
+def truncate_line(truncate_at, string_line):
+
+    byte_count= 0
+    for char in string_line:
+        utf8_byte_len= len(char.encode('utf-8'))
+        if (byte_count+ utf8_byte_len) > truncate_at:
+            break
+        byte_count+= utf8_byte_len
+
+    ba_string= bytearray(string_line, 'utf-8')
+    final_string= str(ba_string[:byte_count], encoding='utf-8')
+
+    return final_string
+
 if __name__ == "__main__":
-    print("hello world")
+    with open('cases', 'rb') as read_file:
+        read_line= read_file.readlines()
+        for line in read_line:
+            print(truncate_line(int(line[0]), str(line[1:], encoding='utf-8').strip()))
