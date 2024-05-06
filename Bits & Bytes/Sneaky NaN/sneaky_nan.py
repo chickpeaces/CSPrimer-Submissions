96edb0e403c5ada781de8a3a9d41691ec1d3e514
chickpeaces <chickpeaces+github@gmail.com>
2024-05-05 14:29:07 -1000

    added sneaky_nan.py

---
+++ Bits & Bytes/Sneaky NaN/sneaky_nan.py
@@ -0,0 +1,23 @@
+from math import nan
+import struct
+
+def conceal( six_byte_msg):
+
+    empty_payload_nan= bytearray( struct.pack( "<d", nan))
+    struct.pack_into( "<6s", empty_payload_nan, 0, six_byte_msg.encode('utf-8'))
+
+    return struct.unpack( "<d", empty_payload_nan)[0]
+
+def extract( loaded_nan):
+
+    payload_nan= bytearray( struct.pack( "<d", loaded_nan))
+    payload_msg= struct.unpack_from( "<6s", payload_nan, 0)
+
+    return payload_msg[0].decode('utf-8')
+
+if __name__ == "__main__":
+    sneaky_nan= conceal("hello!")
+    print( sneaky_nan)
+
+    payload= extract( sneaky_nan)
+    print( payload)
